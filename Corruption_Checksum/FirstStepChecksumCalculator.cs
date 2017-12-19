2ef164c7f2677b0ec9e393a7f58ce5ec3eb62efe
Surya Singh <surya.singh@thetrainline.com>
2017-12-19 16:59:47 +0000

    Added logic of spiral memory part 1

--- Corruption_Checksum/FirstStepChecksumCalculator.cs
+++ Corruption_Checksum/FirstStepChecksumCalculator.cs
@@ -36,4 +36,126 @@ namespace Corruption_Checksum
 
         }
     }
+
+
+    public class Rootobject
+    {
+        public Data data { get; set; }
+        public Included[] included { get; set; }
+    }
+
+    public class Data
+    {
+        public string id { get; set; }
+        public string type { get; set; }
+        public Attributes attributes { get; set; }
+        public Relationships relationships { get; set; }
+        public Links links { get; set; }
+    }
+
+    public class Attributes
+    {
+    }
+
+    public class Relationships
+    {
+        public Voidables voidables { get; set; }
+    }
+
+    public class Voidables
+    {
+        public Datum[] data { get; set; }
+        public Meta meta { get; set; }
+    }
+
+    public class Meta
+    {
+        public string count { get; set; }
+    }
+
+    public class Datum
+    {
+        public string id { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Links
+    {
+        public Self self { get; set; }
+        public CreateVoidContext createvoidcontext { get; set; }
+    }
+
+    public class Self
+    {
+        public string href { get; set; }
+        public Meta1 meta { get; set; }
+    }
+
+    public class Meta1
+    {
+        public string method { get; set; }
+        public string accept { get; set; }
+    }
+
+    public class CreateVoidContext
+    {
+        public string href { get; set; }
+        public Meta2 meta { get; set; }
+    }
+
+    public class Meta2
+    {
+        public string method { get; set; }
+        public string accept { get; set; }
+    }
+
+    public class Included
+    {
+        public string id { get; set; }
+        public string type { get; set; }
+        public Attributes1 attributes { get; set; }
+        public Relationships1 relationships { get; set; }
+        public Links1 links { get; set; }
+    }
+
+    public class Attributes1
+    {
+        public Policy[] policies { get; set; }
+    }
+
+    public class Policy
+    {
+        public string policy { get; set; }
+        public bool isVoidable { get; set; }
+        public DateTime expiryTimestampUtc { get; set; }
+        public Reason reason { get; set; }
+    }
+
+    public class Reason
+    {
+        public string code { get; set; }
+        public string description { get; set; }
+    }
+
+    public class Relationships1
+    {
+    }
+
+    public class Links1
+    {
+        public Self1 self { get; set; }
+    }
+
+    public class Self1
+    {
+        public string href { get; set; }
+        public Meta3 meta { get; set; }
+    }
+
+    public class Meta3
+    {
+        public string method { get; set; }
+        public string accept { get; set; }
+    }
+
 }
---
+++ Spiral_Memory/SpiralMemoryPart1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Spiral_Memory
+{
+    [TestFixture]
+    public class Class1
+    {
+        [TestCase(27, 4)]
+        [TestCase(35, 4)]
+        [TestCase(42, 5)]
+        [TestCase(48, 5)]
+        [TestCase(18, 3)]
+        [TestCase(43, 6)]
+        [TestCase(37, 6)]
+        [TestCase(31, 6)]
+        [TestCase(49, 6)]
+        [TestCase(26, 5)]
+        [TestCase(25, 4)]
+        [TestCase(9, 2)]
+        [TestCase(8, 1)]
+        [TestCase(2, 1)]
+        [TestCase(10, 3)]
+        [TestCase(347991, 480)]
+        public void A(int input, int expected)
+        {
+
+            //var input = 27;
+            //var input = 347991;
+            int lastSpiralLength = 0;
+
+            var sqrt = Math.Sqrt(input);
+            
+            if (IsAOddInteger(sqrt))
+            {
+                lastSpiralLength = (int)sqrt;
+            }
+            else if (IsAEvenInteger(sqrt))
+            {
+                lastSpiralLength = (int) sqrt +1;
+            }
+            else
+            {
+                lastSpiralLength = (int)sqrt + 2;
+            }
+
+            var lastSpiralMax = Math.Pow(lastSpiralLength, 2);
+
+            double lastSpiralLengthDividedByTwo = lastSpiralLength / 2;
+            var centrePoint = Math.Floor(lastSpiralLengthDividedByTwo);
+
+            var position = Math.Abs(input - lastSpiralMax);
+
+            var bottomRightPoint = lastSpiralMax;
+            var bottomLeftPoint = lastSpiralMax - (lastSpiralLength-1);
+            var topLeftPoint = bottomLeftPoint - (lastSpiralLength - 1);
+            var topRightPoint = topLeftPoint - (lastSpiralLength - 1);
+
+            var right = topRightPoint - input;
+            var top = topLeftPoint - input;
+            var left = bottomLeftPoint - input;
+            var bottom = bottomRightPoint - input;
+
+            double value = 0;
+            if (right < lastSpiralLength && right >= 0)
+            {
+                value = topRightPoint - centrePoint - input;
+                Console.WriteLine(value + (int)centrePoint);
+            }
+
+            if (top < lastSpiralLength && top >= 0)
+            {
+                value = topLeftPoint - centrePoint - input;
+                Console.WriteLine(Math.Abs(value) + (int)centrePoint);
+            }
+
+            if (left < lastSpiralLength && left >= 0)
+            {
+                value = bottomLeftPoint - centrePoint - input;
+                Console.WriteLine(Math.Abs(value) + (int)centrePoint);
+            }
+
+            if (bottom < lastSpiralLength && bottom >= 0)
+            {
+                value = bottomRightPoint - centrePoint - input;
+                Console.WriteLine(Math.Abs(value) + (int)centrePoint);
+            }
+
+            Assert.That((int)(Math.Abs(value) + (int)centrePoint), Is.EqualTo(expected));
+        }
+
+        private static bool IsAEvenInteger(double sqrt)
+        {
+            return Math.Floor(sqrt) % 2 == 0;
+        }
+
+        private static bool IsAOddInteger(double sqrt)
+        {
+            return sqrt % 1 == 0 && sqrt % 2 == 1;
+        }
+    }
+}
